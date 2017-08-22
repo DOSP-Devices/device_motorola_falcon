72c101d30e31fdcc27e2fecbb50da3743d15847d
Kukun Mishra <kukun7610@gmail.com>
2017-08-22 17:50:05 +0530

    Update and rename aosp_falcon.mk to dosp_falcon.mk

--- dosp_falcon.mk
+++ dosp_falcon.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
 # Inherit some common Aosp stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/dosp/common.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := aosp_falcon
+PRODUCT_NAME := dosp_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
